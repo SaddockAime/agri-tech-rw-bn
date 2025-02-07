652906f22dc3f077d7ab8b1f9ffab4744c0adc3b
Saddock Kabandana <aimegetz@gmail.com>
2025-02-07 10:26:50 +0200

    initial

---
+++ src/databases/config/config.ts
@@ -0,0 +1,43 @@
+/* eslint-disable comma-dangle */
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const commonDatabaseConfig = {
+  dialect: "postgres",
+};
+
+const sequelizeConfig = {
+  development: {
+    ...commonDatabaseConfig,
+    url: process.env.DATABASE_URL_PRO,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+  test: {
+    ...commonDatabaseConfig,
+    url: process.env.DATABASE_URL_TEST,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+  production: {
+    ...commonDatabaseConfig,
+    url: process.env.DATABASE_URL_PRO,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+};
+
+module.exports = sequelizeConfig;
